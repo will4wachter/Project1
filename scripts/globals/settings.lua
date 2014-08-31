afc3d68f551f7ffe8dc2623004ccbc273d727729
will4wachter <will4wachter@gmail.com>
2014-08-31 05:08:06 +0000

    Gil and Buffs

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -28,7 +28,7 @@ ENABLE_ASA  = 1;
 -- CHARACTER CONFIG
 INITIAL_LEVEL_CAP = 50; --The initial level cap for new players.  There seems to be a hardcap of 255.
 MAX_LEVEL = 75; -- Level max of the server, works by disabling Maat quests.
-START_GIL = 50; --Amount of gil given to newly created characters.
+START_GIL = 500000; --Amount of gil given to newly created characters.
 START_INVENTORY = 80; --Starting inventory and satchel size.  Ignores values < 30.  Do not set above 80!
 OPENING_CUTSCENE_ENABLE = 0; --Set to 1 to enable opening cutscenes, 0 to disable.
 SUBJOB_QUEST_LEVEL = 18; --Minimum level to accept either subjob quest.  Set to 0 to start the game with subjobs unlocked.
