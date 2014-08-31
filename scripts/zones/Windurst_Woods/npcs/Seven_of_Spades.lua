702d4ef335fb1bf36f0573e7306447d9b717def6
William <will4wachter@gmail.com>
2014-08-31 11:19:04 -0400

    Starting NPC Update

--- scripts/zones/Windurst_Woods/npcs/Seven_of_Spades.lua
+++ scripts/zones/Windurst_Woods/npcs/Seven_of_Spades.lua
@@ -18,16 +18,28 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-	player:addItem(11811);
-	player:addItem(10293);
-	player:addItem(15793);
-	player:messageSpecial(ITEM_OBTAINED,11811);
-	player:messageSpecial(ITEM_OBTAINED,10293);
-	player:messageSpecial(ITEM_OBTAINED,15793);
+
+	if(player:hasItem(11811,10293,15793) == false) then
+		player:addItem(11811);
+		player:addItem(10293);
+		player:addItem(15793);
+		player:messageSpecial(ITEM_OBTAINED,11811);
+		player:messageSpecial(ITEM_OBTAINED,10293);
+		player:messageSpecial(ITEM_OBTAINED,15793);
+	end
+	if(player:hasKeyItem(8,9,138) == false) then
+		player:addKeyItem(8);
+		player:addKeyItem(9);
+		player:addKeyItem(138);
+		player:messageSpecial(KEYITEM_OBTAINED,138);
+		player:messageSpecial(KEYITEM_OBTAINED,8);
+		player:messageSpecial(KEYITEM_OBTAINED,9);
+	end
 	player:addFame(BASTOK,  BAS_FAME*30);
 	player:addFame(SANDORIA,SAN_FAME*30);
 	player:addFame(WINDURST,WIN_FAME*30);
 	player:addFame(NORG,    NORG_FAME*30);
+	player:addFame(JEUNO,	JEUNO_FAME*30);
 end;
 
 -----------------------------------
