4504ee0c5d046a9818e6a479c428e8874f682d39
William <will4wachter@gmail.com>
2014-08-31 17:50:30 -0400

    Relic/Mythic

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Amajal.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Amajal.lua
@@ -22,26 +22,26 @@ end;
 
 function onTrigger(player,npc)
 	player:showText(npc,PHAMELISE_OPEN_DIALOG);
-stock = {0x4a35,100000000,		-- Burtgang
-	 0x4a34,100000000,		-- Vajra
-	 0x4a33,100000000,		-- Murgleis
-	 0x4a32,100000000,		-- Laevateinn
-	 0x4a31,100000000,		-- Yagrush
-	 0x4a30,100000000,		-- Glanzfaust
-	 0x4a2f,100000000,		-- Conqueror
-	 0x4a2e,100000000,		-- Tupsimati
-	 0x4a2d,100000000,		-- Terpsichore
-	 0x4a36,100000000,		-- Liberator
-	 0x4a37,100000000,		-- Aymur
-	 0x4a38,100000000,		-- Carnwenhan
-	 0x4a39,100000000,		-- Gastraphetes
-	 0x4a3a,100000000,		-- Kogarasumaru
-	 0x4a3b,100000000,		-- Nagi
-	 0x4a3c,100000000,		-- Ryunohige
-	 0x4a3d,100000000,		-- Nirvana
-	 0x4a3e,100000000,		-- Tizona
-	 0x4a3f,100000000,		-- Death Penalty
-	 0x4a40,100000000}		-- Kenkonken
+stock = {0x4a35,250000000,		-- Burtgang
+	 0x4a34,250000000,		-- Vajra
+	 0x4a33,250000000,		-- Murgleis
+	 0x4a32,250000000,		-- Laevateinn
+	 0x4a31,250000000,		-- Yagrush
+	 0x4a30,250000000,		-- Glanzfaust
+	 0x4a2f,250000000,		-- Conqueror
+	 0x4a2e,250000000,		-- Tupsimati
+	 0x4a2d,250000000,		-- Terpsichore
+	 0x4a36,250000000,		-- Liberator
+	 0x4a37,250000000,		-- Aymur
+	 0x4a38,250000000,		-- Carnwenhan
+	 0x4a39,250000000,		-- Gastraphetes
+	 0x4a3a,250000000,		-- Kogarasumaru
+	 0x4a3b,250000000,		-- Nagi
+	 0x4a3c,250000000,		-- Ryunohige
+	 0x4a3d,250000000,		-- Nirvana
+	 0x4a3e,250000000,		-- Tizona
+	 0x4a3f,250000000,		-- Death Penalty
+	 0x4a40,250000000}		-- Kenkonken
  
 showShop(player, STATIC, stock);
 
--- scripts/zones/Lower_Jeuno/npcs/Rakuru-Rakoru.lua
+++ scripts/zones/Lower_Jeuno/npcs/Rakuru-Rakoru.lua
@@ -23,22 +23,22 @@ function onTrigger(player,npc)
 	
 player:showText(npc,PHAMELISE_OPEN_DIALOG);
 
-stock = {0x3ADE,100000000,		-- Aegis
-	 0x4764,100000000,		-- Excal
-	 0x475e,100000000,		-- Mandau
-	 0x4758,100000000,		-- Sphari
-	 0x476a,100000000,		-- Ragnarok
-	 0x4770,100000000,		-- Gutter
-	 0x4776,100000000,		-- Bravura
-	 0x477c,100000000,		-- Gungnir
-	 0x4782,100000000,		-- Apocalypse
-	 0x4788,100000000,		-- Kikoku
-	 0x478e,100000000,		-- Amano
-	 0x4794,100000000,		-- Mjollnir
-	 0x479a,100000000,		-- Claustrum
-	 0x47a0,100000000,		-- Annihilator
-	 0x47a6,100000000,		-- Gjallarhorn
-	 0x47ac,100000000}		-- Yochi
+stock = {0x3ADE,250000000,		-- Aegis
+	 0x4764,250000000,		-- Excal
+	 0x475e,250000000,		-- Mandau
+	 0x4758,250000000,		-- Sphari
+	 0x476a,250000000,		-- Ragnarok
+	 0x4770,250000000,		-- Gutter
+	 0x4776,250000000,		-- Bravura
+	 0x477c,250000000,		-- Gungnir
+	 0x4782,250000000,		-- Apocalypse
+	 0x4788,250000000,		-- Kikoku
+	 0x478e,250000000,		-- Amano
+	 0x4794,250000000,		-- Mjollnir
+	 0x479a,250000000,		-- Claustrum
+	 0x47a0,250000000,		-- Annihilator
+	 0x47a6,250000000,		-- Gjallarhorn
+	 0x47ac,250000000}		-- Yochi
  
 showShop(player, STATIC, stock);
 end; 
