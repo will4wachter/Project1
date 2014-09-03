ec2fcc6601b23b0f3e6a9a6e5c73951cd4a0cf77
William <will4wachter@gmail.com>
2014-09-03 17:57:32 -0400

    Ninja Tool NPC

--- scripts/zones/Lower_Jeuno/npcs/Bogharg.lua
+++ scripts/zones/Lower_Jeuno/npcs/Bogharg.lua
@@ -19,9 +19,22 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
-player:startEvent(0x002D);
-end; 
 
+stock = {0x14bc,2500,			-- Uchitake
+	 0x14bd,2500,		-- Excal
+	 0x14be,2500,		-- Mandau
+	 0x14bf,2500,
+	 0x14c0,2500,
+	 0x14c1,2500,
+	 0x14c2,2500,
+	 0x14c3,2500,
+	 0x14c4,2500,
+	 0x14c5,2500,
+	 0x14c6,2500,
+	 0x14c7,2500}		-- Yochi
+ 
+showShop(player, STATIC, stock);
+end; 
 -----------------------------------
 -- onEventUpdate
 -----------------------------------
