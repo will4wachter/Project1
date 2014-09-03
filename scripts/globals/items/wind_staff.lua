4b25576eabcbb86a38fce824441a965c08752620
William <will4wachter@gmail.com>
2014-09-02 20:52:18 -0400

    Elemental Staff Additional Effects

---
+++ scripts/globals/items/wind_staff.lua
@@ -0,0 +1,31 @@
+-----------------------------------------
+-- ID: 17549
+-- Item: Wind Staff
+-- Additional Effect: Wind Damage
+-----------------------------------------
+require("scripts/globals/status");
+
+-----------------------------------
+-- onAdditionalEffect Action
+-----------------------------------
+function onAdditionalEffect(player,target,damage)
+    local chance = 90;
+    if (target:getMainLvl() > player:getMainLvl()) then
+        chance = chance - 5 * (target:getMainLvl() - player:getMainLvl())
+        chance = utils.clamp(chance, 5, 95);
+    end
+    if (math.random(0,99) >= chance) then 
+        return 0,0,0;
+    else
+        local diff = player:getStat(MOD_INT) - target:getStat(MOD_INT);
+        if (diff > 20) then
+            diff = 20 + (diff - 20) / 2;
+        end
+        local damage = diff + (player:getMainLvl() - target:getMainLvl()) + damage/2;
+        local params = {};
+        params.bonusmab = 0;
+        params.includemab = false;
+        damage = finalMagicNonSpellAdjustments(player,target,ELE_WIND,damage);
+        return SUBEFFECT_WIND_DAMAGE, 163, player:addHP(-damage);
+    end
+end;
