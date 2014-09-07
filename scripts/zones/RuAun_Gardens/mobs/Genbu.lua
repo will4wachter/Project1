e3cff89bbb4fe4d59301869682a1e41b14e2ea3f
William <will4wachter@gmail.com>
2014-09-07 17:17:06 -0400

    Genbu Gil

--- scripts/zones/RuAun_Gardens/mobs/Genbu.lua
+++ scripts/zones/RuAun_Gardens/mobs/Genbu.lua
@@ -17,5 +17,6 @@ end;
 -----------------------------------
 function onMobDeath(mob, killer)
 	killer:showText(mob,SKY_GOD_OFFSET + 6);
+	killer:addGil(math.random(1000000,5000000));
 	GetNPCByID(17310097):hideNPC(120);
 end;
